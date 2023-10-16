cc16f051e8eefbad54f6ba073719227d139fee07
Dominik Bacher <dominikbacher@icloud.com>
2023-10-16 03:20:47 +0200

    modularized code with logging and model/data saves

--- src/main.py
+++ src/main.py
@@ -1,39 +1,50 @@
+# coding: utf-8
+import logging
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from dill import dump, load
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from sklearn.pipeline import Pipeline
+
+from data.make_dataset import main as make_dataset
+from data.make_dataset import read_train_test_data
 import os
 import dotenv
+
+log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_fmt)
+logger = logging.getLogger(__name__)
 
 project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
 dotenv_path = os.path.join(project_dir, '.env')
 dotenv.load_dotenv(dotenv_path)
-
 data_path = os.getenv("DATA_PATH")
-data = pd.read_csv(data_path)
-data = data.drop(columns="total_bedrooms")
-data_train, data_test = train_test_split(data, test_size=0.33, random_state=0)
-
-# Select X and y values (predictor and outcome)
-X_train = data_train.drop(columns="median_house_value")
-y_train = data_train["median_house_value"]
-X_test = data_test.drop(columns="median_house_value")
-y_test = data_test["median_house_value"]
-
+processed_path = os.getenv("PROCESSED_PATH")
+model_path = os.getenv("MODEL_PATH")
+if not os.path.exists(model_path):
+    os.mkdir(model_path)
+model_file_name = model_path + "/LinearRegression.pkl"
+make_dataset(data_path, processed_path)
+X_train, X_test, y_train, y_test = read_train_test_data(processed_path)
+# Modelling
+# Pipeline Definition
 sc = StandardScaler()
 lin_reg = LinearRegression()
 pipeline_mlr = Pipeline([("data_scaling", sc), ("estimator", lin_reg)])
+# Model Fit
 pipeline_mlr.fit(X_train, y_train)
+logger.debug(f"saving model at {model_file_name}")
+with open(model_file_name, "wb") as f:
+    dump(pipeline_mlr, f)
+
+# Model Evaluation
+with open(model_file_name, "rb") as f:
+    pipeline_mlr = load(f)
 predictions_mlr = pipeline_mlr.predict(X_test)
+# Test score
 pipeline_mlr.score(X_test, y_test)
-
-print("MAE", metrics.mean_absolute_error(y_test, predictions_mlr))
-print("MSE", metrics.mean_squared_error(y_test, predictions_mlr))
-print("RMSE", np.sqrt(metrics.mean_squared_error(y_test, predictions_mlr)))
-print("Explained Var Score", metrics.explained_variance_score(y_test, predictions_mlr))
-
-
-# This Notebook Shows a simple modelling experiment. We will use this base for building our machine Learning Project.
+logger.info(f"MAE {metrics.mean_absolute_error(y_test, predictions_mlr)}")
+logger.info(f"MSE {metrics.mean_squared_error(y_test, predictions_mlr)}")
+logger.info(f"RMSE {np.sqrt(metrics.mean_squared_error(y_test, predictions_mlr))}")
+logger.info(f"Explained Var Score {metrics.explained_variance_score(y_test, predictions_mlr)}")
